5a219088eb33f72753789f4603ca4dee786c9e04
Robert Dale Smith <robert@robertdalesmith.com>
2019-06-04 03:35:14 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+RobertDaleSmith.com 2019
+==========
+Yet another basic personal brand site.
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    compress: false,
+    port: 3000,
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: { 
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        } 
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'RobertDaleSmith.com',
+      template: './src/index.html'
+    }),
+  ]
+};
