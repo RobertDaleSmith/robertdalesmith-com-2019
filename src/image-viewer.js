1290523f4b24182f75d925eeb5f97b756ca9d3f7
Robert Dale Smith <robert@robertdalesmith.com>
2019-06-19 13:39:17 -0500

    adds other images dots to viewer

--- src/image-viewer.js
+++ src/image-viewer.js
@@ -3,49 +3,76 @@
 import template from './views/partials/image-viewer.dust';
 
 function bindImages(className) {
-  document.querySelectorAll(className || '.thumbnail').forEach(element => {
-    // bing click event to thumbnail images
-    element.addEventListener('click', event => {
-      event.preventDefault();
+  const thumbs = document.querySelectorAll(className || '.thumbnail');
+  const getFullPath = path => path.replace('-thumb', '-full');
 
-      // get full image path from clicked thumbnail
-      var src = element.getAttribute('src') || '';
-      src = src.replace('-thumb', '-full');
+  function clickDotEvent(event) {
+    // remove active state from all dots
+    const otherImageDots = document.querySelectorAll('.image_other');
+    otherImageDots.forEach(dot => dot.className = 'image_other');
 
-      // opens full images in new tab if narrow viewport
-      if (window.innerWidth < 1024) {
-        return window.open(src, '_blank');
-      }
+    // set clicked dot to active and update preview src path
+    const thisDot = event.toElement;
+    thisDot.className = 'image_other active';
+    const thisSrc = thisDot.getAttribute('data-src');
+    document.querySelector('.image_preview').setAttribute('src', thisSrc);
+  }
 
-      dust.render(template, { src }, (err, result) => {
-        // generate elements from rendered html string
-        const div = document.createElement('div');
-        div.innerHTML = result;
-        const viewerElement = div.children[0];
+  function closeViewer(event) {
+    // only close on esc keydowns
+    if (event.type === 'keydown' && event.keyCode !== 27) return;
 
-        const closeEvent = e => {
-          // only close on esc keydowns
-          if (e.type === 'keydown' && e.keyCode !== 27) return;
+    // checks for existing image viewer
+    const openViewer = document.querySelector('.image_viewer');
+    if (openViewer) {
+      // removes image viewer and keydown listener
+      document.body.removeChild(openViewer);
+      document.removeEventListener('keydown', closeViewer);
+    }
+  }
 
-          // checks for existing image viewer
-          const openViewer = document.querySelector('.image_viewer');
-          if (openViewer) {
-            // removes image viewer and keydown listener
-            document.body.removeChild(openViewer);
-            document.removeEventListener('keydown', closeEvent);
-          }
-        };
+  function openViewer(event) {
+    event.preventDefault();
 
-        // bind clost events to mark, x button, and esc keydown
-        viewerElement.querySelector('.close_button').addEventListener('click', closeEvent);
-        viewerElement.querySelector('.image_mask').addEventListener('click', closeEvent);
-        document.addEventListener('keydown', closeEvent);
+    // get clicked images src path
+    const clickedImg = event.toElement;
+    let src = clickedImg.getAttribute('src');
+    src = getFullPath(src);
 
-        // add image viewer to body
-        document.body.appendChild(viewerElement);
-      });
+    // opens full images in new tab if narrow viewport
+    if (window.innerWidth < 1024) return window.open(src, '_blank');
+
+    // sets clicked images matching dot active
+    const images = [...thumbs].map(thumb => {
+      const thisSrc = getFullPath(thumb.getAttribute('src'));
+      return {
+        active: (thisSrc === src) || undefined,
+        src: thisSrc,
+      };
     });
-  });
+
+    // generate elements from rendered html string
+    dust.render(template, { src, images }, (err, html) => {
+      const div = document.createElement('div');
+      div.innerHTML = html;
+      const viewer = div.children[0];
+
+      // bind clost events to mark, x button, and esc keydown
+      viewer.querySelector('.close_button').addEventListener('click', closeViewer);
+      viewer.querySelector('.image_mask').addEventListener('click', closeViewer);
+      document.addEventListener('keydown', closeViewer);
+
+      // add image viewer to body
+      document.body.appendChild(viewer);
+
+      // bind click event to all other image dots
+      const otherImageDots = document.querySelectorAll('.image_other');
+      otherImageDots.forEach(dot => dot.addEventListener('click', clickDotEvent));
+    });
+  }
+
+  // bing click event to thumbnail images
+  thumbs.forEach(element => element.addEventListener('click', openViewer));
 }
 
 module.exports = {
